bb70a95b5ab6d2423b40387875ccac1e13053b69
onur kuş <onurkus74@gmail.com>
2022-03-18 16:56:54 +0300

    Credit Card Integrated

---
+++ Business/Concrete/CreditCardManager.cs
@@ -0,0 +1,50 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CreditCardManager : ICreditCardService
+    {
+        private ICreditCardDal _creditCardDal;
+
+        public CreditCardManager(ICreditCardDal creditCardDal)
+        {
+            _creditCardDal = creditCardDal;
+        }
+
+        public IResult Add(CreditCard creditCard)
+        {
+            _creditCardDal.Add(creditCard);
+            return new SuccessResult("Kart Eklendi");
+        }
+
+        public IResult Delete(CreditCard creditCard)
+        {
+            _creditCardDal.Delete(creditCard);
+            return new SuccessResult("Kart Silindi");
+        }
+
+        public IDataResult<List<CreditCard>> GetAll()
+        {
+            var result = _creditCardDal.GetAll();
+            return new SuccessDataResult<List<CreditCard>>(result);
+        }
+
+        public IDataResult<List<CreditCard>> GetAllByCustomerId(int customerId)
+        {
+            var result = _creditCardDal.GetAll(c => c.CustomerId == customerId);
+            return new SuccessDataResult<List<CreditCard>>(result);
+        }
+
+        public IDataResult<CreditCard> GetById(int id)
+        {
+            var result = _creditCardDal.Get(c => c.Id == id);
+            return new SuccessDataResult<CreditCard>(result);
+        }
+    }
+}
